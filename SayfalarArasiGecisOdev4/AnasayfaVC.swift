e57755668e82314ec2d0311c298fed89479a929d
Erbil Can Keleş <64902576+SKYLINE24@users.noreply.github.com>
2023-09-18 17:11:57 +0300

    ödev4 tamamlandı

--- SayfalarArasiGecisOdev4/AnasayfaVC.swift
+++ SayfalarArasiGecisOdev4/AnasayfaVC.swift
@@ -11,9 +11,6 @@ class AnasayfaVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
--- SayfalarArasiGecisOdev4/BSayfasiVC.swift
+++ SayfalarArasiGecisOdev4/BSayfasiVC.swift
@@ -12,7 +12,6 @@ class BSayfasiVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
